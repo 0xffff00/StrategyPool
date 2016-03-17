fd5f6b194f768338ed07a92f3ca206d3aa25d758
heng mao <184772181@qq.com>
2016-03-17 10:06:50 +0800

    策略仓库的建立

---
+++ StrategyPool/StrategyPool/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+##############################################
+策略仓库的建立。
+作者：毛衡
+版本：v1.0.0
+日期：2016年3月17日。
+备注：从期权跨期价差（calendar spread）出发，构建
+自己的策略仓库。今后可以不限于期权策略。
+##############################################
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
